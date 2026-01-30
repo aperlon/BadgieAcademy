fd75e6e99a5ebdf1b8417ce19751b28f5ce29da1
SETSAIL WORLD S.L. <setsailworlds.l.@B67826990.local>
2026-01-30 20:36:38 +0100

    Update image 1.1 and viewport to 87dvh

--- components/SubsectionModal.tsx
+++ components/SubsectionModal.tsx
@@ -114,7 +114,7 @@ export const SubsectionModal = ({
             initial={{ y: "100%" }}
             animate={{
               y: 0,
-              height: isExpanded ? "85dvh" : "50dvh"
+              height: isExpanded ? "87dvh" : "50dvh"
             }}
             exit={{ y: "100%" }}
             transition={{ type: "spring", damping: 30, stiffness: 300 }}
